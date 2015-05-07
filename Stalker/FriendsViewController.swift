d6e4baf38ca9e75315e74b8b865ac91e8873e5dc
Elwin Stephan <elwin.stephan@gmail.com>
2015-05-07 16:54:39 +0200

    Implemented Friend Query

--- Stalker/FriendsViewController.swift
+++ Stalker/FriendsViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 import Parse
 
-class FriendsViewController: UITableViewController {
+class FriendsViewController: UITableViewController, UITableViewDelegate {
+    
+    var users: [PFUser]?
     
     override func viewDidLoad() {
         title = "Stalker"
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Logout", style: UIBarButtonItemStyle.Plain, target: self, action: "logout")
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
         
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        refreshTableView()
+    }
+    
+    func refreshTableView() {
+        
+        let query = PFQuery(className: "_User")
+        query.findObjectsInBackgroundWithBlock { (objects: [AnyObject]?, error: NSError?) -> Void in
+            if error == nil {
+                self.users = objects as? [PFUser]
+                self.tableView.reloadData()
+            }
+        }
     }
     
     func logout() {
@@ -31,4 +50,30 @@ class FriendsViewController: UITableViewController {
         self.presentViewController(loginViewController, animated: true, completion: nil)
     }
     
+    //MARK: TableView Data Source
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let count = users?.count {
+            return count
+        } else {
+            return 0
+        }
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "friendsCell")
+        cell.textLabel?.text = users?[indexPath.row].username
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+    }
 }
